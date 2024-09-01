5a5468c025dcfe499f4019524ccea2d9b18402f3
cmorellh <c.morell.h@gmail.com>
2024-09-01 08:14:19 -0400

    Update login form, added css for styling in file.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import { lusitana } from '@/app/ui/fonts';
+// UI
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { Button } from './button';
+
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+import { lusitana } from '@/app/ui/fonts';
+
+import "./login-form.css"
+
 // Authentication
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
@@ -18,43 +23,36 @@ export default function LoginForm() {
     undefined,
   );
 
-
   return (
     <form action={formAction} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+      <div className="form_main_div">
+        <h1 className={`${lusitana.className} form_h1`}>
           Please log in to continue.
         </h1>
-        <div className="w-full">
+        <div className="input_divs">
           <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
+            <label className="email_label" htmlFor="email">
               Email
             </label>
-            <div className="relative">
+            <div className="input_divs">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer email_input"
                 id="email"
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <AtSymbolIcon className="atsymbolicon" />
             </div>
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
+          <div className="password_div">
+            <label className="password_label" htmlFor="password">
               Password
             </label>
-            <div className="relative">
+            <div className="input_divs">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer password_input"
                 id="password"
                 type="password"
                 name="password"
@@ -62,22 +60,18 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <KeyIcon className="keyicon" />
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+        <Button className="button" aria-disabled={isPending}>
+          Log in <ArrowRightIcon className="login_arrow" />
         </Button>
-        <div className="flex h-8 items-end space-x-1"
-             aria-live="polite"
-             aria-atomic="true"
-            >
-
+        <div className="error_msg_div space-x-1" aria-live="polite" aria-atomic="true">
             {errorMessage && (
               <>
-                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                <p className="text-sm text-red-500">{errorMessage}</p>
+                <ExclamationCircleIcon className="exclamation_icon" />
+                <p className="exclamation_p">{errorMessage}</p>
               </>
             )}
         </div>
