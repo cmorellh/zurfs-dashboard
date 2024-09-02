39940dbd88bcdf106f25eb55a02bac84dceee9da
cmorellh <c.morell.h@gmail.com>
2024-09-02 07:47:21 -0400

    Updated dashboard overview, invoices and layout to use styles in file instead of tailwind.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,15 +1,15 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-
+import "./layout.css";
 
 export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-        <div className="w-full flex-none md:w-64">
+    <div className="main_div">
+        <div className="sidenav_div">
         <SideNav />
         </div>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        <div className="children_div">{children}</div>
     </div>
     );
 }
