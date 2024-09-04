2b5dd6f179d096f0e92e1dc0d6f93ffa906ed69f
cmorellh <c.morell.h@gmail.com>
2024-09-04 06:02:09 -0400

    Update Dashboard design for ZurfsForecasts.

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,11 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { ZurfsIcon } from '@heroicons/react/23/outline';
+
+import {
+  GlobeAltIcon,
+  CpuChipIcon
+} from '@heroicons/react/24/outline';
+
 import { lusitana } from '@/app/ui/fonts';
 
 import './acme-logo.css'
@@ -6,8 +13,9 @@ import './acme-logo.css'
 export default function AcmeLogo() {
   return (
     <div className={`${lusitana.className} logo_div`}>
-      <GlobeAltIcon className="globealticon" />
-      <p className="logo_text">Acme</p>
+      <CpuChipIcon className="globealticon" />
+      <p className="logo_text">Zurfs Forecasts</p>
     </div>
   );
 }
+
