0b5428ce657a6e3ac3277c42625b8acf7c33ceeb
cmorellh <c.morell.h@gmail.com>
2024-09-05 10:36:09 -0400

    Update zurfsforecasts side navigation and links.

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,21 +5,25 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  CommandLineIcon,
+  ChartBarIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+
+import "./nav-links.css"
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
+    name: '🌊 ZurfsUp',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: '༄｡° Metrics', href: '/dashboard/customers', icon: ChartBarIcon },
 ];
 
 export default function NavLinks() {
@@ -32,17 +36,15 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            // Replacing classname with clsx that lets us conditionally select the stile ;-)
-            // className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'zurfslink_standard',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'zurfslink_triggered': pathname === link.href,
               },
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <p className="paragraph_linkname">{link.name}</p>
           </Link>
         );
       })}
