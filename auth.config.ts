ce90ba37a77b966cccc0ebdd2150a4ce218e10be
cmorellh <c.morell.h@gmail.com>
2024-09-08 19:34:55 -0400

    Update auth.config in branch for type.

--- auth.config.ts
+++ auth.config.ts
@@ -6,15 +6,15 @@ export const authConfig = {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }: { auth: Session | null, request: { nextUrl: NextRequest } }) {
+    authorized({ auth, request }: { auth: Session | null, request: NextRequest }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard');
       
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/dashboard', request.nextUrl));
       }
       return true;
     },
