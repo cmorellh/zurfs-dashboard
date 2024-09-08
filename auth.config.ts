f99f51c7243f695343c003aa2f7bd3aa41dc1dc1
cmorellh <c.morell.h@gmail.com>
2024-09-08 19:30:32 -0400

    Update auth.config in branch for type.

--- auth.config.ts
+++ auth.config.ts
@@ -1,13 +1,15 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+import type { NextAuthConfig, Session } from 'next-auth';
+import type { NextRequest } from 'next/server';
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    authorized({ auth, request: { nextUrl } }: { auth: Session | null, request: { nextUrl: NextRequest } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
